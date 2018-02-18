0a47e5f9545e327908543c8a330f7b838827912e
Ignacio <ignacio.past@gmail.com>
2018-02-18 22:15:06 +0000

    reading functionality stable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { NFC, Ndef } from '@ionic-native/nfc';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    NFC,
+    Ndef,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { NFC, Ndef } from '@ionic-native/nfc';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class  HomePage {
 
-  constructor(public navCtrl: NavController) {
+  nfcListener;
+  mySubscription;
 
+  constructor(public navCtrl: NavController, private platform: Platform, private nfc: NFC, private ndef: Ndef) {
+    
+  }
+
+  ionViewDidLoad() {
+    this.bindNfcListenter();
+  }
+
+  // FIXME: There is no need to bind this function to a button in Android. Need to check if is relevant do it in ios
+  async bindNfcListenter() {
+    // TODO: check with ios device if this make nfc work in ios
+    if (this.platform.is('ios')) {
+      let bSession;
+      try { 
+        bSession = await this.nfc.beginSession();
+      }
+      catch (err) {console.log("err init session", err);}
+    }
+    
+    try {
+      this.nfcListener = await this.nfc.addNdefListener();
+
+    } catch (errAdd) {
+      console.log("Err adding the listener", errAdd);
+    }
+
+  }
+
+  startRead() {
+    try {if (this.mySubscription) this.mySubscription.unsubscribe();} catch(err) {};
+    this.mySubscription = this.nfcListener.subscribe((event) => {
+      console.log('received ndef message. the tag contains: ', event.tag);
+      console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+    
+      // let message = this.ndef.textRecord('Hello world');
+      // this.nfc.share([message]).then(onSuccess).catch(onError);
+    });
+  }
+
+  stopReading(){
+    this.mySubscription.unsubscribe();
   }
 
 }
